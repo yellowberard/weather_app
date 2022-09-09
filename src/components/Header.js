9cb35be8cc677237e7b006ccda64bce601fe99f1
Mayank Goel <goel.mayank235@gmail.com>
2022-09-10 03:17:26 +0530

    Added header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+
+   return(
+    <div>
+    <div className="appName">
+        <h1 className='text-primary'>Weather App</h1>
+    </div>
+    <div className="searchBar"></div>
+</div>
+   )
+}
+
+export default Header;
